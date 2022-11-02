3ac27d23b9482ea714ba93b7831b24fa0795d17f
Atikingi <n.igumentsev@gmail.com>
2022-11-02 23:15:48 +0300

    Add all logic for library backend

---
+++ src/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema( {
+    name: {
+        type: String,
+        required: true,
+        minLength: 2
+    },
+    surname: {
+        type: String,
+        required: true,
+        minLength: 2
+    },
+    username: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    books: {
+        type: Array,
+        required: false,
+        minLength: false
+    }
+})
+
+module.exports = mongoose.model('user', userSchema);
